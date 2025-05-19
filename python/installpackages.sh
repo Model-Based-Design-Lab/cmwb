b9247d03c9e302eee172b1337ca3a2913403b700
Marc Geilen <m.c.w.geilen@tue.nl>
2025-05-19 08:37:00 +0200

    small fixes layout and container version

--- python/installpackages.sh
+++ python/installpackages.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 source compmod_env/bin/activate
+python3 -m pip install setuptools
 python3 -m pip install $1/finitestateautomata/
 python3 -m pip install $1/markovchains/
 python3 -m pip install $1/dataflow/
